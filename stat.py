3887f43235345c7ae51c3c9c0fa6f05abe24f64b
Alex Hagen <alexhagen6@gmail.com>
2019-01-11 08:57:33 -0800

    Working on stats propagation stuff.

---
+++ stat.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class sample(object):
+    """Docstring."""
+    def __init__(self):
+        """Docstring."""
+        pass
+
+    def mean(self):
+        """Docstring."""
+        pass
+
+    def std(self):
+        """Docstring."""
+        pass
